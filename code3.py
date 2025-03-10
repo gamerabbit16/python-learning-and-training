513499157a82738571b45596417aed1d3784942d
Gamerabbit <jordapaul16@gmail.com>
2025-03-10 20:36:11 +0100

    Add random fact generator in code3.py

---
+++ code3.py
@@ -0,0 +1,15 @@
+import random
+a = random.randint(0, 5)
+
+if a == 0:
+  print("Flamingos turn pink from eating shrimp.")
+elif a == 1:
+  print("he only food that doesn\'t spoil is honey.")
+elif a == 2:
+  print("Shrimp can only swim backwards.")
+elif a == 3:
+  print("A taste bud\'s life span is about 10 days.")
+elif a == 4:
+  print("It is impossible to sneeze while sleeping.")
+elif a == 5:
+  print("it is illegal to sing off in north calor")
