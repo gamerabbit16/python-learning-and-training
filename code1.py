16aafb36a10cc3265948cba647afc6fc9bc1961b
Gamerabbit <jordapaul16@gmail.com>
2025-03-10 19:50:26 +0100

    Add rating evaluation logic to code1.py

---
+++ code1.py
@@ -0,0 +1,12 @@
+rating = 4.7
+
+if rating > 4.5 :
+  print("Extraordinary")
+elif rating > 4 :
+  print("Excelent")
+elif rating > 3:
+  print("good")
+elif rating > 2 :
+  print("fair")
+else:
+  print("poor")
