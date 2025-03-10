42b03dccfb242776109b621b2d35f5d35554314e
Gamerabbit <jordapaul16@gmail.com>
2025-03-10 19:54:29 +0100

    Implement rating evaluation logic in code1.py

---
+++ code2.py
@@ -0,0 +1,12 @@
+grade = int(input("your grade ?"))
+
+if grade == 9:
+  print("freshman")
+elif grade == 10:
+  print("Sophomore")
+elif grade == 11:
+  print("junior")
+elif grade == 12:
+  print("senior")
+else:
+  print("tbd")
